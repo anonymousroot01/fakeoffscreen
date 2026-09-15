202a323701ae5be7c5c752c4f11001b1f399f5c8
ANONYMOUS <sekkelemakarna@gmail.com>
2026-09-15 19:21:19 +0300

    Implement BootReceiver to start service on boot

--- app/src/main/java/com/example/fakeoffscreen/BootReceiver.kt
+++ app/src/main/java/com/example/fakeoffscreen/BootReceiver.kt
@@ -1,1 +1,23 @@
+package com.example.fakeoffscreen
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED ||
+            intent.action == "android.intent.action.QUICKBOOT_POWERON"
+        ) {
+            val prefs = context.getSharedPreferences("settings", Context.MODE_PRIVATE)
+            if (prefs.getString("pattern", null) != null) {
+                val i = Intent(context, BlackScreenService::class.java)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                    context.startForegroundService(i)
+                else
+                    context.startService(i)
+            }
+        }
+    }
+}
